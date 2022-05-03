64beed06ef56e9bafbfa7d487325c34a968582d6
unknown <kentleung@gmail.com>
2022-05-03 17:49:56 -0400

    depreciated

--- js/main.js
+++ js/main.js
@@ -8,7 +8,19 @@ class TileObject {
 
 // Create listeners for every tiles
 const tile = document.querySelectorAll('.tile')
-tile.forEach((x) => x.addEventListener('click', tileClick))
+// tile.forEach((x) => x.addEventListener('click', tileClick))
+
+document.querySelector('#t0').addEventListener('click', tileClick)
+document.querySelector('#t1').addEventListener('click', tileClick)
+document.querySelector('#t2').addEventListener('click', tileClick)
+document.querySelector('#t3').addEventListener('click', tileClick)
+document.querySelector('#t4').addEventListener('click', tileClick)
+document.querySelector('#t5').addEventListener('click', tileClick)
+document.querySelector('#t6').addEventListener('click', tileClick)
+document.querySelector('#t7').addEventListener('click', tileClick)
+document.querySelector('#t8').addEventListener('click', tileClick)
+
+
 
 // Create objects for every tile
 const tileArr = []
@@ -16,8 +28,8 @@ for(i = 0; i < tile.length; i++) {
   tileArr[i] = new TileObject()
 }
 
-function tileClick() {
-  console.log(`clicked`)
+function tileClick(i) {
+  console.log(`${i} clicked`)
 }
 
 
