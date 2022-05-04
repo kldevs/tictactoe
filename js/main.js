0df8fbda393e26c8c39736cbfa9aaa2c57705e35
unknown <kentleung@gmail.com>
2022-05-04 15:00:46 -0400

    Consolidated object fucntions from DOM functions

--- js/main.js
+++ js/main.js
@@ -1,127 +1,119 @@
-// Class for TileObject
-class TileObject {
-  constructor() {
-    this.value = ""
+/// TTTBoard object
+class TTTBoard{
+  constructor(){
+    this.tile = ["", "", "", "", "", "", "", "", ""]
+    this.currentPlayer = "X"
+    this.gameEnded = false
+    this.gameWinner = ""
   }
-  setValue(currentTurn){
-    this.value = currentTurn
+
+  setTile(currentPlayer, i){
+    if(this.gameEnded === true){
+      console.log(`Game has ended`)
+    } else if(this.tile[i] === "X" || this.tile[i] === "O"){
+      console.log('Box taken. Please choose another box.')
+    } else {
+      this.tile[i] = currentPlayer
+      this.checkGameEnd()
+      this.changePlayer()
+    }
   }
-}
 
-let currentTurn = 'X'
-// Selects all tiles
-const tileAll = document.querySelectorAll('.tile')
-// tile.forEach((x) => x.addEventListener('click', tileClick))
-
-// Create objects for every tile
-const tile = []
-for (let i = 0; i < tileAll.length; i++) {
-  tile[i] = new TileObject()
-}
-resetGame()
-function tileClick0() {
-  onClick(0)
-}
-function tileClick1() {
-  onClick(1)
-}
-function tileClick2() {
-  onClick(2)
-}
-function tileClick3() {
-  onClick(3)
-}
-function tileClick4() {
-  onClick(4)
-}
-function tileClick5() {
-  onClick(5)
-}
-function tileClick6() {
-  onClick(6)
-}
-function tileClick7() {
-  onClick(7)
-}
-function tileClick8() {
-  onClick(8)
-}
-
-function onClick(i) {
-  if(tile[i].value === "X" || tile[i].value === "O") {
-    console.log('Box taken. Please choose another box.')
-  } else {
-    tile[i].setValue(currentTurn)
-    console.table(tile)
-    checkGameEnd()
-    changeTurn()
-    drawBoard()
+  // check winning conditions
+  checkGameWin(){
+    if (this.tile[0] === this.currentPlayer && this.tile[1] === this.currentPlayer && this.tile[2] === this.currentPlayer || 
+        this.tile[3] === this.currentPlayer && this.tile[4] === this.currentPlayer && this.tile[5] === this.currentPlayer || 
+        this.tile[6] === this.currentPlayer && this.tile[7] === this.currentPlayer && this.tile[8] === this.currentPlayer || 
+        this.tile[0] === this.currentPlayer && this.tile[3] === this.currentPlayer && this.tile[6] === this.currentPlayer || 
+        this.tile[1] === this.currentPlayer && this.tile[4] === this.currentPlayer && this.tile[7] === this.currentPlayer || 
+        this.tile[2] === this.currentPlayer && this.tile[5] === this.currentPlayer && this.tile[8] === this.currentPlayer || 
+        this.tile[0] === this.currentPlayer && this.tile[4] === this.currentPlayer && this.tile[8] === this.currentPlayer || 
+        this.tile[2] === this.currentPlayer && this.tile[4] === this.currentPlayer && this.tile[6] === this.currentPlayer){
+      this.gameWinner = this.currentPlayer
+      return true
+    }
   }
-}
-
-document.querySelector('#t0').addEventListener('click', tileClick0)
-document.querySelector('#t1').addEventListener('click', tileClick1)
-document.querySelector('#t2').addEventListener('click', tileClick2)
-document.querySelector('#t3').addEventListener('click', tileClick3)
-document.querySelector('#t4').addEventListener('click', tileClick4)
-document.querySelector('#t5').addEventListener('click', tileClick5)
-document.querySelector('#t6').addEventListener('click', tileClick6)
-document.querySelector('#t7').addEventListener('click', tileClick7)
-document.querySelector('#t8').addEventListener('click', tileClick8)
-
-function checkGameEnd(){
-  // winning conditions
-  if (tile[0].value === currentTurn && tile[1].value === currentTurn && tile[2].value === currentTurn || 
-    tile[3].value === currentTurn && tile[4].value === currentTurn && tile[5].value === currentTurn || 
-    tile[6].value === currentTurn && tile[7].value === currentTurn && tile[8].value === currentTurn || 
-    tile[0].value === currentTurn && tile[3].value === currentTurn && tile[6].value === currentTurn || 
-    tile[1].value === currentTurn && tile[4].value === currentTurn && tile[7].value === currentTurn || 
-    tile[2].value === currentTurn && tile[5].value === currentTurn && tile[8].value === currentTurn || 
-    tile[0].value === currentTurn && tile[4].value === currentTurn && tile[8].value === currentTurn || 
-    tile[2].value === currentTurn && tile[4].value === currentTurn && tile[6].value === currentTurn){
-  document.querySelector('h2').innerText = `Player ${currentTurn} wins!`
-  resetGame()
+  
   // game ends in a tie with no moves left
-  } else if (tile[0].value !== null && 
-    tile[1].value !== null && 
-    tile[2].value !== null && 
-    tile[3].value !== null && 
-    tile[4].value !== null && 
-    tile[5].value !== null && 
-    tile[6].value !== null && 
-    tile[7].value !== null && 
-    tile[8].value !== null){
-  document.querySelector('h2').innerText = `Tie Game!`
-  resetGame()
-  } 
-}
-
-// resets all tiles back to null value
-function resetGame(){
-  for (let i = 0; i < tile.length; i++){
-    tile[i].value = null  
+  checkGameTie(){
+    if (this.tile.filter((x) => x === "").length === 0 && !this.checkGameWin()){
+      return true
+    }
   }
-  console.log('New game started')
-  printTurn()
+
+  checkGameEnd(){
+    if(this.checkGameWin()){
+      this.gameEnded = true
+      console.log(`Player ${this.gameWinner} Wins!`)
+    } else if(this.checkGameTie()){
+      this.gameEnded = true
+      console.log(`Tie Game!`)
+    }
+
+  }
+
+  // resets all tiles back to "" value
+  resetGame(){
+    for (let i = 0; i < this.tile.length; i++){
+      this.tile[i] = ""  
+    }
+    this.gameEnded = false
+    console.log('New game started')
+  }
+
+  // change player turn
+  changePlayer(){
+    this.currentPlayer = (this.currentPlayer === 'X' ? 'O' : 'X')
+  }
 }
 
-function changeTurn(){
-  currentTurn = (currentTurn === 'X' ? 'O' : 'X')
-  printTurn()
+// Code above will handle logic
+// Code below will manipulate DOM
+
+const board = new TTTBoard()
+
+function printWinner(){
+  document.querySelector('h2').innerText = `Player ${board.winner} wins!`
 }
 
-function printTurn(){
-  if (currentTurn === "X"){
+function printTie(){
+  document.querySelector('h2').innerText = `Tie Game!`
+}
+
+function printCurrentPlayer(){
+  if (board.currentPlayer === "X"){
     document.querySelector('h3').innerText = "Player X's turn"
   }
-  if (currentTurn === "O"){
+  if (board.currentPlayer === "O"){
     document.querySelector('h3').innerText = "Player O's turn"
   }
 }
 
-//draw values to tile id
-function drawBoard() {
-  for (let i = 0; i < tile.length; i++){
-    document.querySelector(`#t${i}`).innerText = tile[i].value
+function printBoard(){
+  for (let i = 0; i < board.tile.length; i++){
+    document.querySelector(`#t${i}`).innerText = board.tile[i]
   }
 }
+
+function onClick(i) {
+    board.setTile(board.currentPlayer, i)
+    printBoard()
+    printCurrentPlayer()
+    // printWinner()
+    // printTie()
+
+}
+
+// for(i = 0; i < board.tile.length; i++){
+//   document.querySelector(`#t${i}`).addEventListener('click', onClick(i))
+// }
+
+document.querySelector('#t0').addEventListener('click', () => onClick(0))
+document.querySelector('#t1').addEventListener('click', () => onClick(1))
+document.querySelector('#t2').addEventListener('click', () => onClick(2))
+document.querySelector('#t3').addEventListener('click', () => onClick(3))
+document.querySelector('#t4').addEventListener('click', () => onClick(4))
+document.querySelector('#t5').addEventListener('click', () => onClick(5))
+document.querySelector('#t6').addEventListener('click', () => onClick(6))
+document.querySelector('#t7').addEventListener('click', () => onClick(7))
+document.querySelector('#t8').addEventListener('click', () => onClick(8))
